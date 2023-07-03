27f81fe8047f31cb28edfe85e839b2b1932b461c
shailesh pendam <29425549+shaileshpendams@users.noreply.github.com>
2023-07-03 21:43:04 +0530

    Update even_numbers.py

--- even_numbers.py
+++ even_numbers.py
@@ -1,7 +1,7 @@
 def only_the_even(numbers):
     return [num for num in numbers if num % 2 == 0]
 
-input_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # input = input('list')
-output_list = only_the_even(input_list)
-print(output_list)
+output = only_the_even(input)
+print(output)
